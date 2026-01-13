c5b8718d4b47cdf14b446d1739c238facb2fc266
Baochau916 <chuchu916elearning@gmail.com>
2026-01-13 11:46:12 +0700

    add project4

---
+++ projects/project-4/Mental Health 101/story_content/5XUiHiMlAjL_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5XUiHiMlAjL_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.104.35448.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.500%0D%0AModule%206.%20Seeking%20Professional%20Help%0D%0A%0D%0A"}]})
